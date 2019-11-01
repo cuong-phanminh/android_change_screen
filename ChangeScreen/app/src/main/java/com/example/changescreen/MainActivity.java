2e1d77e29754a1b9b71b153dcdd910667f73ebe2
Phan Minh Cuong <cuong.phan@pn.com.vn>
2019-11-01 11:45:31 +0700

    firstcommit

---
+++ ChangeScreen/app/src/main/java/com/example/changescreen/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.changescreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnChange;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnChange = findViewById(R.id.button_change);
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSecondScreen();
+            }
+        });
+    }
+
+    //cach chuyen screen
+    private void openSecondScreen(){
+        Intent intent=new Intent(this, MainSecondActivity.class);
+        startActivity(intent);
+    }
+}
